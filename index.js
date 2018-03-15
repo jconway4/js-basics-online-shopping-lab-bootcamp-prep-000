58e17c43cf68ca1d2029286bece9e11cff8613ce
jconway4 <jconwa4@illinois.edu>
2018-03-15 01:36:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    cart = [];
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    returnString = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return 
   }
 }
